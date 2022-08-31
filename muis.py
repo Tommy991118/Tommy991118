d0c593af3add438fcdf33d010f74af611f040b15
Tommy_Amisulashvili <90957089+Tommy991118@users.noreply.github.com>
2022-08-31 16:34:40 +0200

    Delete muis.py

--- muis.py
+++
@@ -1,9 +0,0 @@
-import pyautogui as py
-import time
-
-
-time.sleep(1) #Geeft tijd om de muis op de gewenste positie te plaatsen.
-print(py.position()) #Geeft de (x,y) coördinaat van de positie van de muis.
-
-
-
